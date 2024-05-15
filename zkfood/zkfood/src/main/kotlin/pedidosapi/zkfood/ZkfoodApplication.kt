0881f431b79180035c4ae9343f57ba1f7e7dadad
AlexandreDSptech <alexandre.murata@sptech.school>
2024-05-15 01:31:30 -0300

    feat: endpoints levemente arrumados

---
+++ zkfood/zkfood/src/main/kotlin/pedidosapi/zkfood/ZkfoodApplication.kt
@@ -0,0 +1,11 @@
+package pedidosapi.zkfood
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZkfoodApplication
+
+fun main(args: Array<String>) {
+	runApplication<ZkfoodApplication>(*args)
+}
---
+++ zkfood/zkfood/src/test/kotlin/pedidosapi/zkfood/ZkfoodApplicationTests.kt
@@ -0,0 +1,13 @@
+package pedidosapi.zkfood
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZkfoodApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
