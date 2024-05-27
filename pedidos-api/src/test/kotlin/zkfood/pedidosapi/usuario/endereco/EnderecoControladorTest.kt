3e4ceac12574a4797fedd6d2142adc341170f5e7
enzo.gama <stanganelli899@gmail.com>
2024-05-27 13:18:29 -0300

    classe teste de TelefoneControlador

--- pedidos-api/src/test/kotlin/zkfood/pedidosapi/usuario/endereco/EnderecoControladorTest.kt
+++ pedidos-api/src/test/kotlin/zkfood/pedidosapi/usuario/endereco/EnderecoControladorTest.kt
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity
 import zkfood.pedidosapi.usuario.telefone.telefoneDado.Telefone
 import zkfood.pedidosapi.usuario.telefone.telefoneDado.TelefoneCadastro
 
-class TelefoneControladorTest {
+class TelefoneControladorTests {
 
     @Mock
     private lateinit var telefoneServico: TelefoneServico
