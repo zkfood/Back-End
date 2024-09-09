a038b6eb22f339d77dec88c6c9a2fb68c1818084
AlexandreDSptech <alexandre.murata@sptech.school>
2024-09-08 23:17:20 -0300

    fix

--- pedidos-api/src/main/kotlin/zkfood/pedidosapi/avaliacao/AvaliacaoRepositorio.kt
+++ pedidos-api/src/main/kotlin/zkfood/pedidosapi/avaliacao/AvaliacaoRepositorio.kt
@@ -2,7 +2,6 @@ package zkfood.pedidosapi.avaliacao
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.jpa.repository.query.Procedure
 import zkfood.pedidosapi.avaliacao.avaliacaoDado.Avaliacao
 import zkfood.pedidosapi.avaliacao.avaliacaoDado.AvaliacaoId
 
