843cad100e5e704782133bac1ab84650fe92932d
AlexandreDSptech <alexandre.murata@sptech.school>
2024-10-27 19:03:35 -0300

    refactor: usuario e correção de bugs

--- pedidos-api/src/main/kotlin/zkfood/pedidosapi/usuario/usuario/usuarioErros/LoginOuSenhaNaoInformados.kt
+++ pedidos-api/src/main/kotlin/zkfood/pedidosapi/usuario/usuario/usuarioErros/LoginOuSenhaNaoInformados.kt
@@ -3,7 +3,7 @@ package zkfood.pedidosapi.usuario.usuario.usuarioErros
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
-@ResponseStatus(code = HttpStatus.CONFLICT)
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 class LoginOuSenhaNaoInformados:RuntimeException {
     constructor():super("Login ou Senha não informados");
 }
